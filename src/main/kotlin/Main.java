3a1d169142c3d543bc4ad7b7bfd341d5f500873d
Faisal Amir <faisalamircs@gmail.com>
2022-04-01 16:56:01 +0700

    Update and rename Main.kt to Main.java

--- src/main/kotlin/Main.java
+++ src/main/kotlin/Main.java
@@ -11,6 +11,10 @@
  *
  */
 
-fun main(args: Array<String>) {
-    println("Damn you boys")
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World !!!");
+    }
 }
